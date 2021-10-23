771977bbf1594f23f93d18b0ed9e43b528739562
Mohan Konem <mohan.konem@gmail.com>
2021-10-23 15:35:53 +0530

    6th commit

--- SDP/settings.py
+++ SDP/settings.py
@@ -133,16 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# BASEPATH = Path(__file__).resolve().parent.parent
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+BASEPATH = Path(__file__).resolve().parent.parent
 
-# STATIC_ROOT = BASEPATH.joinpath('static')
-# BASEPATH.joinpath('media')
+STATIC_ROOT = BASEPATH.joinpath('static')
+BASEPATH.joinpath('media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
